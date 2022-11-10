160b5947f624fbe87b0aa573fba6d460372d5132
tinytan7 <aish.stha07@gmail.com>
2022-11-10 11:12:58 +0545

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import mainRoutes from "./routes/main.js";
+import userRoutes from "./routes/userRoutes.js"
 
 //Intialize the app with express
 const app = express();
@@ -10,9 +11,8 @@ app.use(express.json());
 
 //routes
 app.use("/api", mainRoutes);
+app.use("/api/user", userRoutes);
 
-
-// Define the port where the server should listen
-app.listen(8800, () => {
+// Defi, () => {
     console.log("connected to the server")
 })
